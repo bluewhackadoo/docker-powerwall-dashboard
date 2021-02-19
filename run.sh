6c3786a70e725d40d1e8419cf108e69050ed686f
Eric Kratzer <erickr@microsoft.com>
2021-02-19 10:45:08 -0800

    Fixed another missed parsing issue

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ userrec=$(grep -o -P '(?<=UserRecord\s)([\w|\-|\d|\=]+)' /tmp/cookie.txt)
 TSLA_COOKIE=$(echo {\"Cookie\" = \"AuthCookie=$auth\; UserRecord=$userrec\"})
 
 # Replace the cookie in powerwall.conf since influxd gets confused by the chars if we try to do env expansion from TSLA_COOKIE
-sed -i -e 's/ headers.*/headers = \'"$TSLA_COOKIE"'/g' /etc/telegraf/telegraf.d/powerwall.conf
+sed -i -e 's/    headers.*/    headers = \'"$TSLA_COOKIE"'/g' /etc/telegraf/telegraf.d/powerwall.conf
 
 # Start influx
 /usr/bin/influxd -config /etc/influxdb/influxdb.conf &
